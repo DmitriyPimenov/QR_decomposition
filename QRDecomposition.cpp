6da49415a29e4bdd4b6b80a0bab745aa9d0a2b94
Dmitry Pimenov <dimapim99@gmail.com>
2023-11-23 02:20:45 +0300

    Correctly but too slowly algorithm

--- QRDecomposition.cpp
+++ QRDecomposition.cpp
@@ -12,11 +12,11 @@ void pr(std::vector<std::vector<double>>& A) { // Не забыть удалит
     std::cout << std::endl;
 }
 
-void QR(std::vector<std::vector<double>>& A, size_t n) {
+void QR(std::vector<std::vector<double>>& A, size_t n, size_t ii) {
     std::vector<std::vector<double>> U(n, std::vector<double> (n, 0.));
     for (size_t i = 0; i < n; ++i)
         U[i][i] = 1.;
-    for (size_t k = 0; k < n - 1; ++k) {
+    for (size_t k = 0; k < n; ++k) {
         std::vector<double> a(n - k);
         for (size_t i = k; i < n; ++i)
             a[i - k] = A[i][k];
@@ -34,10 +34,16 @@ void QR(std::vector<std::vector<double>>& A, size_t n) {
         xNorm = sqrt(xNorm);
         for (double& i : x)
             i /= xNorm;
+        if (xNorm < 1e-10) {
+            x[0] = A[k][k] > 0 ? 1. : -1.;
+            for (size_t i = 1; i < n - k; ++i) {
+                x[i] = 0.;
+            }
+        }
 
         std::vector<std::vector<double>> u(n - k, std::vector<double> (n - k, 0.));
         for (size_t i = 0; i < n - k; ++i)
-            u[i][i] = 1;
+            u[i][i] = 1.;
         for (size_t i = 0; i < n - k; ++i) {
             for (size_t j = 0; j < n - k; ++j) {
                 u[i][j] -= 2 * x[i] * x[j];
@@ -79,6 +85,16 @@ void QR(std::vector<std::vector<double>>& A, size_t n) {
                 U[i][j] = tmp[i][j];
             }
         }
+        if (ii == 0) {
+            std::cout << "x" << std::endl;
+            for (double i : x)
+                std::cout << i << ' ';
+            std::cout << std::endl;
+            std::cout << "U" << std::endl;
+            pr(U);
+            std::cout << "A" << std::endl;
+            pr(A);
+        }
     }
 
     std::vector<std::vector<double>> R = A, Q(n, std::vector<double>(n));
@@ -91,14 +107,10 @@ void QR(std::vector<std::vector<double>>& A, size_t n) {
         for (size_t j = 0; j < n; ++j) {
             A[i][j] = 0.;
             for (size_t t = 0; t < n; ++t) {
-                A[i][j] += R[i][t] * Q[t][j]; // Еуе умножается на U^t
+                A[i][j] += R[i][t] * Q[t][j];
             }
         }
     }
-    std::cout << "R" << std::endl;
-    pr(R);
-    std::cout << "Q" << std::endl;
-    pr(Q);
 
     std::vector<std::vector<double>> checkA(n, std::vector<double> (n, 0.));
     std::vector<std::vector<double>> QU = checkA;
@@ -110,10 +122,18 @@ void QR(std::vector<std::vector<double>>& A, size_t n) {
             }
         }
     }
-    std::cout << "checkA" << std::endl;
-    pr(checkA);
-    std::cout << "QU" << std::endl;
-    pr(QU);
+    if (ii == 0) {
+        std::cout << "R" << std::endl;
+        pr(R);
+        std::cout << "Q" << std::endl;
+        pr(Q);
+        std::cout << "checkA" << std::endl;
+        pr(checkA);
+        std::cout << "QU" << std::endl;
+        pr(QU);
+        std::cout << "A" << std::endl;
+        pr(A);
+    }
 }
 
 void leftMultiplication(std::vector<std::vector<double>>& A, const double cosPhi, const double sinPhi,
@@ -121,6 +141,13 @@ void leftMultiplication(std::vector<std::vector<double>>& A, const double cosPhi
     double xi = A[i][k], xj = A[j][k];
     A[i][k] = xi * cosPhi - xj * sinPhi;
     A[j][k] = xi * sinPhi + xj * cosPhi;
+}
+
+void rightMultiplication(std::vector<std::vector<double>>& A, const double cosPhi, const double sinPhi,
+                        const size_t i, const size_t j, const size_t k) {
+    double xi = A[k][i], xj = A[k][j];
+    A[k][i] = xi * cosPhi - xj * sinPhi;
+    A[k][j] = xi * sinPhi + xj * cosPhi;
 }
 
 bool getEigenvalues(size_t n, std::vector<std::vector<double>>& A,
@@ -137,12 +164,15 @@ bool getEigenvalues(size_t n, std::vector<std::vector<double>>& A,
             for (size_t k = j; k < n; ++k) { // Lemma 5
                 leftMultiplication(A, cosPhi, sinPhi, j + 1, i, k); // Lemma 4
             }
+            for (size_t k = 0; k < n; ++k) {
+                rightMultiplication(A, cosPhi, sinPhi, j + 1, i, k);
+            }
         }
     }
 
     pr(A);
-    for (size_t i = 0; i < 1; ++i)
-        QR(A, n);
+    for (size_t i = 0; i < 1000; ++i)
+        QR(A, n, i);
     pr(A);
 
     for (size_t i = 0; i < n; ++i) {
